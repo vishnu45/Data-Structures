d47c6dcb21b9937d49f294717eeaefc3803b33aa
vishnu4v5 <vishnu4v5@gmail.com>
2016-05-21 03:56:09 -0400

    adding stack, initial commit

---
+++ C++/Stack/stack_usingArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Stack
+{
+	int top;
+	int capacity;
+	int *array;
+};
+
+struct Stack *createStack(int capacity)
+{
+	
+}
+
+int main()
+{
+
+	return 0;
+}
