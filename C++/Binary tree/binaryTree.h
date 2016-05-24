45d1fa455582558cb44ed8d0faf14548bf7686dd
vishnu4v5 <vishnu4v5@gmail.com>
2016-05-24 06:02:26 -0400

    added b.tree traversal - pre, in, post - order

--- C++/Binary tree/binaryTree.h
+++ C++/Binary tree/binaryTree.h
@@ -5,20 +5,55 @@
 
 using namespace std;
 
-struct binaryTree
+// binary tree structure
+struct BinaryTree
 {
 	int data;
-	struct binaryTree *left;
-	struct binaryTree *right;
+	struct BinaryTree *left;
+	struct BinaryTree *right;
 };
 
-struct binaryTree* newNode(int value)
+// create a binary tree node
+struct BinaryTree* NewNode(int value)
 {
-	struct binaryTree *tObject = new binaryTree;
+	struct BinaryTree *tObject = new BinaryTree;
 	tObject -> data = value;
 	tObject -> left = NULL;
 	tObject -> right = NULL;
 	return tObject;
+}
+
+// binary tree traversal: inorder
+void InOrder(struct BinaryTree *root)
+{
+	if(root == NULL)
+		return;	
+	InOrder(root -> left);
+	cout << root -> data << " ";
+	InOrder(root -> right);	
+	return;
+}
+
+// binary tree traversal: preorder
+void PreOrder(struct BinaryTree *root)
+{
+	if(root == NULL)
+		return;	
+	cout << root -> data << " ";
+	PreOrder(root -> left);		
+	PreOrder(root -> right);	
+	return;
+}
+
+// binary tree traversal: postorder
+void PostOrder(struct BinaryTree *root)
+{
+	if(root == NULL)
+		return;	
+	PostOrder(root -> left);
+	PostOrder(root -> right);
+	cout << root -> data << " ";
+	return;
 }
 
 #endif
--- C++/Binary tree/driver.cpp
+++ C++/Binary tree/driver.cpp
@@ -5,23 +5,47 @@ using namespace std;
 int main()
 {
 	// create root node
-	struct binaryTree *root = newNode(1);
+	struct BinaryTree *root = NewNode(1);
 
 	// add very first left and right leaves
-	root -> left = newNode(2);
-	root -> right = newNode(3);
+	root -> left = NewNode(2);
+	root -> right = NewNode(3);
 
 	// add leaves for the left node
-	root -> left -> left = newNode(4);
-	root -> left -> right = newNode(5);
+	root -> left -> left = NewNode(4);
+	root -> left -> right = NewNode(5);
 
-	/*------------------
-			[1]
-		   /   \
-		 [2]   [3]
-		/   \
-	  [4]   [5]
-	-------------------*/
-	  
+	// add leaves for the right node
+	root -> right -> left = NewNode(6);
+	root -> right -> right = NewNode(7);
+
+	root -> left -> left -> left = NewNode(8);
+	root -> left -> left -> right = NewNode(9);
+
+	root -> left -> right -> left = NewNode(10);
+	root -> left -> right -> right = NewNode(11);
+
+	root -> right -> left -> left = NewNode(12);
+	root -> right -> left -> right = NewNode(13);
+
+	root -> right -> right -> left = NewNode(14);
+	root -> right -> right -> right = NewNode(15);
+
+	/*------------------------
+			   [1]
+		     /     \
+		 [2]         [3]
+	    /   \		/   \
+      [4]   [5]   [6]   [7]
+      | |   | |   | |   | |
+      8-9  10-11 12-13 14-15 
+	---------------------------*/
+
+	InOrder(root);
+	cout << endl;
+	PreOrder(root);
+	cout << endl;
+	PostOrder(root);
+
 	return 0;
 }
