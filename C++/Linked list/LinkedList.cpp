837dc50b032280656a95b20c4cefee3c299413b3
vishnu4v5 <vishnu4v5@gmail.com>
2016-07-26 19:17:28 -0400

    Adding testDriver for linkedlist class implementation

--- C++/Linked list/LinkedList.cpp
+++ C++/Linked list/LinkedList.cpp
@@ -1,1 +1,36 @@
-#include ""
+#include <iostream>
+#include "LinkedList.h"
+using namespace std;
+
+LinkedList::LinkedList()
+{
+	cout << "Initializing linked list ..." << endl;
+	head = NULL;
+	listLength = 0;
+}
+
+void LinkedList::Push(int value)
+{
+	cout << "Inserting value at the front ..." << endl;
+
+	// create the new node
+	struct node *newNode = new node;
+	newNode -> data = value;
+
+	// check if list is empty
+	if (!head)
+	{
+		head = newNode;
+		newNode -> next = NULL;
+	}
+	else
+	{
+		struct node *temp = new node;
+		temp = head;
+		newNode -> next = temp;
+		head = newNode;
+	}
+	listLength++;
+	return;
+}
+
