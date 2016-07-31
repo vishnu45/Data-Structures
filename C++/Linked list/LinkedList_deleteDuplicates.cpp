b212d4a2a508ca067f8eee6228a3a419fb7c5fbe
vishnu4v5 <vishnu4v5@gmail.com>
2016-07-30 21:10:26 -0400

    Adding global gitignore

---
+++ C++/Linked list/LinkedList_deleteDuplicates.cpp
@@ -0,0 +1,24 @@
+#include "LinkedList.h"
+#include <iostream.h>
+using namespace std;
+
+LinkedList *RemoveDuplicates(LinkedList *inputList)
+{
+	LinkedList *temp;
+	temp = inputList;
+
+	return temp;
+}
+
+int main()
+{
+	// Creating linkedlist with duplicate enteries
+	LinkedList inputList;
+
+	// display list
+
+	// remove duplicates from list
+
+	// display list
+	return 0;
+}
