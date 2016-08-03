35cb9bcac59834da7680ed55be82b139f1f8a57b
vishnu4v5 <vishnu4v5@gmail.com>
2016-08-03 09:59:07 -0400

    Adding Linkedlist: delete duplicates in linked list

--- C++/Linked list/deleteDuplicates.cpp
+++ C++/Linked list/deleteDuplicates.cpp
@@ -1,13 +1,39 @@
 #include "LinkedList.h"
-#include <iostream.h>
+#include <iostream>
 using namespace std;
 
-LinkedList *RemoveDuplicates(LinkedList *inputList)
+// function to remove duplicates in list
+void RemoveDuplicates(LinkedList *inputList)
 {
-	LinkedList *temp;
-	temp = inputList;
+	struct node* temp1 = new node;
+	temp1 = inputList -> GetHead();
+	struct node* temp2 = new node;
+	struct node* prev = new node;
 
-	return temp;
+	while (temp1)
+	{
+		temp2 = temp1 -> next;
+		prev = temp1;
+
+		while (temp2)
+		{				
+			// if a duplicate of temp1 is found
+			if (temp1 -> data == temp2 -> data)
+			{
+				prev -> next = temp2 -> next;
+				temp2 = temp2 -> next;
+				// here advance the temp2 node but not the prev node
+				continue;
+			}
+
+			// advance temp2 to next node in the list
+			prev = temp2;
+			temp2 = temp2 -> next;
+		}
+		temp1 = temp1 -> next;
+	}
+
+	return;
 }
 
 int main()
@@ -16,9 +42,24 @@ int main()
 	LinkedList inputList;
 
 	// display list
+	inputList.Append(1);
+	inputList.Append(1);
+	inputList.Append(2);
+	inputList.Append(2);
+	inputList.Append(3);
+	inputList.Append(2);
+	inputList.Append(5);
+	inputList.Append(1);
+	inputList.Append(6);
+	inputList.Append(1);
+
+	inputList.PrintList();
 
 	// remove duplicates from list
+	RemoveDuplicates(&inputList);
 
 	// display list
+	inputList.PrintList();
+
 	return 0;
 }
