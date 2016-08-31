e818b20edfc08be94b647f872e2cde441769ab76
vishnu4v5 <vishnu4v5@gmail.com>
2016-08-30 22:56:27 -0400

    Adding-J: LinkedList: Doubly linked list initial commit

---
+++ Java/LinkedList/DList.java
@@ -0,0 +1,9 @@
+public class DList {
+
+	// --------------------	MAIN --------------------------
+	public static void main(String[] args) {
+		System.out.println("---------------------"
+			+ " Starting program execution "
+			+ "---------------------");
+	}
+}
