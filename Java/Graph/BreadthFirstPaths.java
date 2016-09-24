00c8e288d107f07feb43ca9e229f5679eff8df10
vishnu4v5 <vishnu4v5@gmail.com>
2016-09-24 11:31:04 -0400

    ADD-J: Graph - Adding Breadth first search

--- Java/Graph/BreadthFirstPaths.java
+++ Java/Graph/BreadthFirstPaths.java
@@ -1,9 +1,9 @@
 /** -------------------------------------------------------
- *	Depth first search class implementation
+ *	Breadth first search class implementation
 -------------------------------------------------------- */
 
 import java.util.*;
-public class DepthFirstPaths {
+public class BreadthFirstPaths {
 
 	// to mark if the vertex was visited
 	private boolean[] marked;
@@ -15,29 +15,38 @@ public class DepthFirstPaths {
 	/**
 	 *	DFS constructor initializes marked[] and calls dfs()
 	 */
-	public DepthFirstPaths(Graph G, int S) {
+	public BreadthFirstPaths(Graph G, int S) {
 		this.marked = new boolean[G.V()];
 		this.edgeTo = new int[G.V()];
 		this.S = S;
-		dfs(G, this.S);
+		bfs(G, S);
 	}
+
 
 	/**
 	 *	Performs depth first search starting at vertex v
 	 */
-	private void dfs(Graph G, int v) {
-		// mark the current node as visited
+	public void bfs(Graph G, int v) {
+		// mark source vertex as visited
 		marked[v] = true;
-		// go through all the nodes the current node is connected to
-		for ( int w : G.adj(v)) {
-			// if the node is already visited, ignore
-			if (!marked[w]) {
-				// go the unvisited node and record the edge from which it was reached
-				edgeTo[w] = v;
-				// call the dfs on this vertex
-				dfs(G, w);	
-			}			
-		}
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(v);
+
+		// go through all vertices until pq is empty
+		while (!pq.isEmpty()) {
+			// remove vertex stored in front of queue
+			int top = pq.poll();
+			// go through all vertices connected to v
+			for (int w : G.adj(top)) {
+				if (!marked[w]) {
+					// visit the node and mark them as visited
+					edgeTo[w] = top;
+					marked[w] = true;
+					// add to the queue
+					pq.add(w);
+				}
+			}
+		}		
 	}
 
 	/**
@@ -52,25 +61,22 @@ public class DepthFirstPaths {
 	 *	returns the path to the vertex v from the source s
 	 */
 	public Iterable<Integer> pathTo(int v) {
-		// check if there is a path from S to the v
-		if (!hasPathTo(v)) return null;
-
-		// return back a stack of all edgeTo vertices
+		
 		Stack<Integer> path = new Stack<Integer>();
-		int temp = v;
-		while (temp != this.S) {			
-			path.push(temp);
-			temp = edgeTo[temp];
+		// using edgeTo iterate back to the starting position
+		for (int i = v; i != edgeTo[i]; i = edgeTo[i]) {
+			path.push(i);
 		}
 		path.push(this.S);
 		return path;
 	}
 
-	public boolean marked(int v) { return marked[v]; }
-	public int edgeTo(int v) { return edgeTo[v]; }
+
+
 
 	// --------------------- MAIN ------------------------
 	public static void main(String[] args) {
+
 		Graph G1 = new Graph(13);
 		G1.addEdge(0, 5);
 		G1.addEdge(4, 3);
@@ -85,14 +91,14 @@ public class DepthFirstPaths {
 		G1.addEdge(7, 8);
 		G1.addEdge(9, 11);
 		G1.addEdge(5, 3);
-		DepthFirstPaths D = new DepthFirstPaths(G1, 0);	
+		BreadthFirstPaths D = new BreadthFirstPaths(G1, 0);		
 
 		// print out all the paths
 		for (int v = 0; v < G1.V(); v++) {
 			if (!D.hasPathTo(v)) {
 				break;
 			}
-			System.out.print(v + ":");			
+			System.out.print(v + ": ");			
 			for (int w: D.pathTo(v)) {				
 				System.out.print(w + " <- ");
 			}
